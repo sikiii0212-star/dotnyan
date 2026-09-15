396ff5aeee66077d834f00a802f8ac8759e7752a
sikiii0212-star <sikiii0212@gmail.com>
2026-09-16 03:01:46 +0900

    Update service worker cache version to refresh app files

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'dotnyan-v1';
+const CACHE = 'dotnyan-v2';
 const ASSETS = ['./', './index.html', './manifest.webmanifest'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
